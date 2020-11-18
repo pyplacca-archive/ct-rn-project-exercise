367d4c82cfd573a3b92e69f6bd4f348fe249f576
pyPlacca <dhegreatprosperity10@gmail.com>
2020-11-18 14:32:15 +0000

    transform screens folder into a module

---
+++ resources/screens/index.js
@@ -0,0 +1,3 @@
+export { default as AddToCart } from './add-to-cart';
+export { default as OnlineShopping } from './online-shopping';
+export { default as PaymentSuccessful } from './payment-successful';
