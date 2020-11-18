6a82fa89728bc50fd6b02ef73a5ca6f8060058aa
pyPlacca <dhegreatprosperity10@gmail.com>
2020-11-18 14:03:16 +0000

    Create utils.js

---
+++ resources/utils.js
@@ -0,0 +1,21 @@
+const globalStyles = {
+	gapMini: 10,
+	gapNorm: 25,
+	gapMedi: 50,
+	gapMaxi: 100,
+	fszNorm: 16,
+	fszMedi: 22,
+	fszMaxi: 47,
+	primaryColor: '#6C63FF',
+	secColor: '#737373'
+}
+
+const lorem = '' +
+	'Lorem Ipsum is simply a dummy text of the printing and typesetting industry. ' +
+	'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, ' +
+	'when an unknown preinter took a'
+
+export default {
+	globalStyles,
+	lorem
+}
