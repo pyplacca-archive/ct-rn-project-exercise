9f5742d0fb34a2ed5d229a4b0dce9969c574d4f2
pyPlacca <dhegreatprosperity10@gmail.com>
2020-11-25 13:26:42 +0000

    implement screen navigation

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { StatusBar, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { PaymentSuccessful, AddToCart, OnlineShopping } from './src/screens'
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
 	return (
-		<View>
-			<StatusBar style="auto" />
-			<PaymentSuccessful />
-		</View>
+		<NavigationContainer>
+			<Stack.Navigator screenOptions={{headerShown: false}}>
+				<Stack.Screen name="online-shopping" component={OnlineShopping} />
+				<Stack.Screen name="add-to-cart" component={AddToCart} />
+				<Stack.Screen name="payment-successful" component={PaymentSuccessful} />
+			</Stack.Navigator>
+		</NavigationContainer>
 	);
 };
